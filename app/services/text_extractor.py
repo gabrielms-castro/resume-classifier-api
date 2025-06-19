211d9018759775b03fe73328cdc5d8b71ca5877b
gabrielms-castro <gabrielms.castro@gmail.com>
2025-06-19 10:46:21 -0300

    feat: add extract_text() for each TextExtractor subclass

--- app/services/text_extractor.py
+++ app/services/text_extractor.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
+import io
+import re
 
+from abc import ABC, abstractmethod
 from pypdf import PdfReader
 
 
@@ -9,22 +11,33 @@ class TextExtractor(ABC):
         raise NotImplementedError("This method should be implemented by subclasses")
 
 class PDFTextExtractor(TextExtractor):
-    ...
+    def extract_text(self, content):
+        reader = PdfReader(io.BytesIO(content))
+        text_pages = []
+        for page in reader.pages:
+            text = page.extract_text()
+            text_pages.append(text)
+        return clean_output_text("\n".join(text_pages))
+    
 
 class MSWordTextExtractor(TextExtractor):
-    ...
+    def extract_text(self, content):
+        try:
+            import docx
+            doc = docx.Document(io.BytesIO(content))
+            text_pages = []
+            for paragraph in doc.paragraphs:
+                text_pages.append(paragraph.text)
+            return clean_output_text("\n".join(text_pages))
+        
+        except ImportError:
+            raise RuntimeError("docx module not found")
 
 class PlainTextExtractor(TextExtractor):
-    ...
-    
-def read_pdf(file_path):
-    reader = PdfReader(file_path)
-    
-    pdf_txt = []
-    for page in reader.pages:
-        text = page.extract_text()
-        pdf_txt.append(text)
-    return pdf_txt
+    def extract_text(self, content):
+        if isinstance(content, bytes):
+            return clean_output_text(content.decode(encoding='utf-8', errors='ignore'))
+        return ""
 
 def call_text_extractor(file_type):
     extractors = {
@@ -33,3 +46,11 @@ def call_text_extractor(file_type):
         "text/plain": PlainTextExtractor
     }
     return extractors[file_type]()
+
+def clean_output_text(text):
+    if not text:
+        return ""
+    
+    text = re.sub(r'[\n\s\t]+', " ", text)
+    text = re.sub(r'\s{2,}', " ", text)
+    return text.strip()
