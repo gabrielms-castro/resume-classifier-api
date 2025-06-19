6fe1dbdbcefcf7963360520b284736aee9948fd6
gabrielms-castro <gabrielms.castro@gmail.com>
2025-06-19 10:46:55 -0300

    feat(tests): add tests for Text Extractors

---
+++ tests/test_text_extractor.py
@@ -0,0 +1,47 @@
+
+from app.services.text_extractor import call_text_extractor, clean_output_text
+
+def test_pdf_text_extractor_extract_text():
+    print("\n[DEBUG] Testing PDF Text Extractor...")
+    # Simulating the UploadFile object file.read() method
+    with open("tests/files/sample.pdf", "rb") as file:
+        content = file.read()
+        print(f"[DEBUG] Bytes: {len(content)}")
+        
+    extractor = call_text_extractor(file_type="application/pdf")
+    text = extractor.extract_text(content)
+    # text = clean_text(text)
+    print(f"[DEBUG] Extracted Text Sample: {text[:100]}")
+
+    assert isinstance(text, str)
+    assert len(text.strip()) > 0 
+       
+def test_docx_text_extractor_extract_text():
+    print("\n[DEBUG] Testing DOCX Text Extractor...")
+    # Simulating the UploadFile object file.read() method
+    with open("tests/files/sample.docx", "rb") as file:
+        content = file.read()
+        print(f"[DEBUG] Bytes: {len(content)}")
+        
+    extractor = call_text_extractor(file_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+    text = extractor.extract_text(content)
+    # text = clean_text(text)
+    print(f"[DEBUG] Extracted Text Sample: {text[:100]}")
+
+    assert isinstance(text, str)
+    assert len(text.strip()) > 0  
+      
+def test_txt_text_extractor_extract_text():
+    print("\n[DEBUG] Testing TXT Text Extractor...")
+    # Simulating the UploadFile object file.read() method
+    with open("tests/files/sample.txt", "rb") as file:
+        content = file.read()
+        print(f"[DEBUG] Bytes: {len(content)}")
+        
+    extractor = call_text_extractor(file_type="text/plain")
+    text = extractor.extract_text(content)
+    # text = clean_text(text)
+    print(f"[DEBUG] Extracted Text Sample: {text[:100]}")
+
+    assert isinstance(text, str)
+    assert len(text.strip()) > 0    
