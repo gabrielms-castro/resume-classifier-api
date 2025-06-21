f4e0029760f3ee95d038fc17e705a22cc2f88f8d
gabrielms-castro <gabrielms.castro@gmail.com>
2025-06-21 15:48:27 -0300

    feat(API): add lifespan for context manager and implemented data persintency in the upload_resume endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,34 @@
+import subprocess
 from fastapi import FastAPI, File, HTTPException, UploadFile, status
+from contextlib import asynccontextmanager
 
-from app.db.mongo import save
-from app.models.resume import ResumeUploadRequest
-from app.services.classifier import classify_resume
-from app.services.text_extractor import call_text_extractor
+# from app.db.mongo_connection import save
+from app.db.connection_options.connection import DBConnectionHandler
+from app.db.repositories.resumes_repository import ResumesRepository
+from app.models.resume_schemas import ResumeUploadRequest
+from app.services.text_extractor_service import call_text_extractor
 
-app = FastAPI()
+db_handler = DBConnectionHandler()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global db_connection
+    global resume_repository
+    
+    print("[DEBUG] Application startup: Initializing resources...")
+    
+    db_handler.connection_to_db()
+    db_connection = db_handler.get_db_connection()
+    resume_repository = ResumesRepository(db_connection=db_connection)
+    print("[DEBUG] Database connection established:", db_connection)
+    print("[DEBUG] Repository connection established:", resume_repository)
+    
+    yield # app running here
+    
+    print("[DEBUG] Application shutdown: Cleaning up resources...")
+    db_handler.close_connection()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
@@ -21,44 +44,45 @@ async def upload_resume(file: UploadFile = File(...)):
     
     file_type = file.content_type
     if file_type not in extensions:
-        return HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
             detail=f"Unsuported file type: {file_type}. Supported types are: {', '.join(extensions)}"
         )
     
-    content = await file.read()
-    if len(content) == 0:
-        return HTTPException(
+    content_raw = await file.read()
+    if len(content_raw) == 0:
+        raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f"error: File is empty or could not be processed. File size: {len(content)} bytes",
+            detail=f"error: File is empty or could not be processed. File size: {len(content_raw)} bytes",
         )
-        
-    text = call_text_extractor(content, file_type)
-        
-    (label, score) = classify_resume(text)
     
-    document = {
-        "filename": file.filename,
-        "file_size": len(content),
-        "file_type": file.content_type,
-        "classification": {
-            "label": label,
-            "confidence_score": score
-        }
-    }
+    extractor = call_text_extractor(file_type=file_type)
+    content_processed = extractor.extract_text(content_raw)
     
     data = ResumeUploadRequest(
-        file=content,
-        filename=file.filename
+        file=content_raw,
+        file_content=content_processed,
+        filename=file.filename,
+        file_size=len(content_raw),
+        file_type=file.content_type,
+        classification={
+            "label": "Match",
+            "score": 0.8
+        }
     )
     
-    _id = save(document)
-    
-    return {
-        "id": _id,
+    document = data.model_dump()
+    persist_response = resume_repository.insert_document(document)
+    response = {
+        "message": "Resume uploaded successfully",
+        "id": str(persist_response.get("_id")),
         "filename": file.filename,
+        "file_size": len(content_raw),
+        "file_type": file.content_type,
         "classification": {
-            "label": label,
-            "confidence_score": score
-        }
+            "label": "Match",
+            "confidence_score": 0.82
+        }        
     }
+    
+    return response
